9c17520ec136c93f6a7cb2bb171b9b0ad32f3390
Izzy4 <baielaibek@mail.ru>
2022-03-09 23:46:36 +0300

    Dop

--- app/src/main/java/com/example/encard/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/encard/ui/activity/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.encard.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.encard.R;
+import com.example.encard.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
     }
 }
