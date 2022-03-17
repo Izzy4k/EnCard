a4dc80c93ab6f4c012c30966fa9a546911e12ccd
Izzy4 <baielaibek@mail.ru>
2022-03-17 20:39:15 +0300

    GitBrat

--- app/src/main/java/com/example/encard/data/network/model/video/module/VideoModule.java
+++ app/src/main/java/com/example/encard/data/network/model/video/module/VideoModule.java
@@ -1,12 +1,12 @@
 package com.example.encard.data.network.model.video.module;
 
 
+import androidx.annotation.NonNull;
+
 import com.example.encard.data.network.model.image.module.ImageModule;
 import com.example.encard.data.network.model.video.remote.VideoApi;
 import com.example.encard.domain.model.video.repo.VideoStorage;
-import com.example.encard.utils.EndPoints;
 
-import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
@@ -14,9 +14,7 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module(includes = {ImageModule.class})
 @InstallIn(SingletonComponent.class)
@@ -24,12 +22,8 @@ public class VideoModule {
 
     @Provides
     @Singleton
-    public VideoApi videoApi(OkHttpClient okHttpClient) {
-        return new Retrofit.Builder()
-                .baseUrl(EndPoints.BASE_URL_PIXA)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient)
-                .build()
+    public VideoApi videoApi(@NonNull Retrofit retrofit) {
+        return retrofit
                 .create(VideoApi.class);
     }
 
--- app/src/main/java/com/example/encard/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/encard/ui/activity/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     private NavController controller;
     @Inject
     public Pref pref;
-    public Pref pref1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
